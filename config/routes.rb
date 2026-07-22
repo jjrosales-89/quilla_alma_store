79a3cd2a3a2ac8e17c89d2214ab8c9379b46a6be
Juan Jose R.O. <jrosalesortega@academic.rrc.ca>
2026-07-22 14:13:41 -0500

    Add category browsing pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   root "products#index"
   resources :products, only: %i[index show]
+  resources :categories, only: %i[index show]
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
